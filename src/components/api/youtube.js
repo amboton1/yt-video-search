8d609c19724bed3e58b05a02241f5c22d4ad1a79
ammar <ammar.botonjic@convergent-usa.com>
2020-12-20 18:46:31 +0100

    init

--- src/components/api/youtube.js
+++ src/components/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBaNJ_eoSUmnWOnYmsKKJMQn6PGgkAb-48";
+const KEY = "";
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
